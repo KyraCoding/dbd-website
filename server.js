9bf5d285259e3823dddd35f2b5e7960e550b54a3
Glitch (dbdcraft) <none>
2024-08-15 17:25:04 +0000

    🐎🫓 Updated with Glitch

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const fetch = require("node-fetch");
-const cheerio = require("cheerio")
+const cheerio = require("cheerio");
 const fs = require("fs");
 const path = require("path");
 
 const app = express();
 const port = 3000;
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile(__dirname+"/pages/home.html");
 });
 app.get("/api", async (req, res) => {
   const filePath = path.join(__dirname, "data", "perks.json");
@@ -19,25 +19,25 @@ app.get("/api", async (req, res) => {
     }
     try {
       const rawData = JSON.parse(data);
-      const perkData = rawData.perks
-      const tags = ["generator", "aura"]
-      perkData.forEach(perk => {
-        perk.img = `https://cdn.nightlight.gg/img/perks/${perk.img}.png`
-        delete(perk.url)
-        delete(perk.id)
-        delete(perk.tier)
-        perk.tags = []
+      const perkData = rawData.perks;
+      const tags = ["generator", "aura"];
+      perkData.forEach((perk) => {
+        perk.img = `https://cdn.nightlight.gg/img/perks/${perk.img}.png`;
+        delete perk.url;
+        delete perk.id;
+        delete perk.tier;
+        perk.tags = [];
         const $ = cheerio.load(perk.description);
-        perk.description = $.text()
-        tags.forEach(tag =>{
+        perk.description = $.text();
+        tags.forEach((tag) => {
           if (perk.description.indexOf(tag) > -1) {
-            perk.tags.push(tag)
+            perk.tags.push(tag);
           }
-        })
-      })
-      res.type('application/json').json(perkData)
+        });
+      });
+      res.type("application/json").json(perkData);
     } catch (parseErr) {
-      console.log(parseErr)
+      console.log(parseErr);
       res.status(500).json({ error: "Failed to parse JSON" });
     }
   });
